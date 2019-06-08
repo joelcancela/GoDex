0d02536fe075823f6edc2ff5568dc68db78c1c1a
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2019-06-08 20:29:58 +0200

    Updated caught + hotfix caught total count

--- src/components/pokedex/stats-section/stats-section.js
+++ src/components/pokedex/stats-section/stats-section.js
@@ -1,18 +1,18 @@
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { POKEMON_ID_WHITELIST, POKEMON_GO_TOTAL } from '../PokedexConstants'
+import {POKEMON_ID_WHITELIST, POKEMON_GO_TOTAL} from '../PokedexConstants'
 
-let StatsSection = ({ stats }) => {
+let StatsSection = ({stats}) => {
 	const currentTotal = POKEMON_GO_TOTAL + POKEMON_ID_WHITELIST.length;
 
 	return (
 		<div>
 			<h2>Stats</h2>
-			<span>Caught: {stats.caught}/{currentTotal}</span><br />
-			<span>Unavailable: {stats.unavailable}</span><br />
-			<span>Remaining: {currentTotal - stats.unavailable - stats.caught}</span><br />
-			<span>Total: {currentTotal}</span><br />
+			<span>Caught: {stats.caught}/{currentTotal - stats.unavailable}</span><br/>
+			<span>Unavailable: {stats.unavailable}</span><br/>
+			<span>Remaining: {currentTotal - stats.unavailable - stats.caught}</span><br/>
+			<span>Total: {currentTotal}</span><br/>
 		</div>
 	);
 }

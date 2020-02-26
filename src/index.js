d708c31eead52cf2daca924a23d22576905dce74
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2020-02-26 21:19:41 +0100

    Disable service-worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import App from './components/app';
 
 render(<App />, document.getElementById('root'));
-
-serviceWorker.register();

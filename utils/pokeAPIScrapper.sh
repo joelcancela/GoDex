0e23ea86aa604bf39aacfd38e5cf8a0c55337f74
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2021-10-16 18:54:29 +0200

    Updated pokemons lists

--- utils/pokeAPIScrapper.sh
+++ utils/pokeAPIScrapper.sh
@@ -3,7 +3,7 @@
 count=$(curl -s https://pokeapi.co/api/v2/pokemon | jq '.count')
 echo $count
 read
-for i in `seq 1 $count`
+for i in `seq 809 1 $count`
 do
-   curl -s "https://pokeapi.co/api/v2/pokemon/$i" | jq '[.[] | {id:.id, name:.name, type: (if .types[0].slot == 1 then .types[0].type.name else .types[1].type.name end)} ]' >> pokemons.json
+   curl -s "https://pokeapi.co/api/v2/pokemon/$i" | jq '{id:.id, name:.name, type: (if .types[0].slot == 1 then .types[0].type.name else .types[1].type.name end)}' >> pokemons.json
 done

adfc29823163e57901a2abd4ca6baab0a1077c5c
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2021-10-17 17:36:10 +0200

    Brought back serviceWorker to make site PWA compliant again

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
 import App from './components/app';
+import * as serviceWorker from './serviceWorker';
 
 render(<App />, document.getElementById('root'));
+serviceWorker.register();

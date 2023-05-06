55f3b701e9c2feb1e674c4793109760a96308e01
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2023-05-06 17:07:06 +0200

    Added Gen8

--- utils/pokeAPIScrapper.sh
+++ utils/pokeAPIScrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Pokeapi does not have 8th gen data yet !
 count=$(curl -s https://pokeapi.co/api/v2/pokemon | jq '.count')
-echo $count
+startCount=899
+echo "Total Pkmn available: $count, scrap starting from $startCount press Enter to continue"
 read
-for i in `seq 809 1 $count`
+for i in `seq $startCount 1 $count`
 do
    curl -s "https://pokeapi.co/api/v2/pokemon/$i" | jq '{id:.id, name:.name, type: (if .types[0].slot == 1 then .types[0].type.name else .types[1].type.name end)}' >> pokemons.json
 done
